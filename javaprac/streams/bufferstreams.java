c3a269fce389bdf524314c8848b6ae5466096edd
meharehsaan <ehsaan.mehar02@gmail.com>
2023-05-12 01:43:20 +0500

    streams

---
+++ javaprac/streams/bufferstreams.java
@@ -0,0 +1,32 @@
+package streams;
+import java.io.*;
+import java.util.*;
+
+public class bufferstreams {
+    public static void main(String[] args) {
+        try {
+            FileInputStream bis = new FileInputStream("data.txt");
+            BufferedInputStream bis1 = new BufferedInputStream(bis);
+            FileOutputStream bos = new FileOutputStream("data1.txt");
+            BufferedOutputStream bos1 = new BufferedOutputStream(bos);
+
+            try {
+                byte[] buffer = new byte[1024];
+                int bytes;
+                while ((bytes = bis1.read(buffer)) != -1) {
+                    // System.out.println();
+                    bos1.write(buffer, 0, bytes);
+                }
+                System.out.println("\n");
+                bis1.close();
+                bos1.close();
+            } catch (IOException e) {
+                System.out.println("helsklsdk");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not exists");
+        } catch (Exception e) {
+            System.out.println("Exception occurs" + e);
+        }
+    }
+}
---
+++ javaprac/streams/serialization.java
@@ -0,0 +1,38 @@
+package streams;
+import java.util.*;
+import java.io.*;
+
+public class serialization implements Serializable {
+    String name = " ";
+    int phone = 0;
+    transient String address = " ";
+
+    public serialization(String n, int ph, String ad) {
+        name = n;
+        phone = ph;
+        address = ad;
+    }
+
+    public void print() {
+        System.out.println(name + " " + phone + " " + address);
+    }
+
+    public static void main(String[] args) {
+        serialization obj = new serialization("Ehsaan", 537, "hostel");
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("file.txt"));
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("file.txt"));
+            out.writeObject(obj);
+            // System.out.println("some text");
+            serialization ref = (serialization) in.readObject();
+            System.out.println(ref.address + " " + ref.phone + " " + ref.name);
+
+            out.close();
+            in.close();
+        } catch (IOException e) {
+            System.out.println("Exception IO");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Exception class not found");
+        }
+    }
+}
